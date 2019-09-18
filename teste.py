c3f912ddbdace02fedd622679103854732c6effc
AE <alinem2104@gmail.com>
2019-09-18 10:06:26 -0300

    Python part 1

---
+++ teste.py
@@ -0,0 +1,36 @@
+# idade = 19
+
+# if idade > 18:
+#   print("Maior de 18, pode tomar cerveja")
+# elif idade == 18:
+#   print("18 anos")
+# else:
+#   print("Ainda não, só danoninho")
+
+
+# def verrugas():
+#   print("Verrugas")
+#verrugas()
+
+
+# for(let i=0; i<meuarray.length;i++){
+#   console.log(meuarray[i])
+# }
+
+
+# minhaLista = ['a','b','c','d']
+# minhaLista.sort(reverse=True)
+# print(minhaLista)
+
+# for i in minhaLista:
+#   print(i)
+
+
+# for i in range(1,10,2):
+#   print(i)
+
+
+lista1 = [9,4,7,2,5,8]
+print(lista1)
+lista1.sort()
+print(lista1)
